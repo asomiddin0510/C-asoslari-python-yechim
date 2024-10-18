8d2152a6517f65c4d86bf339d7622866ef3a36c6
asomiddin <asom1dd1n0510@gmail.com>
2024-10-18 21:26:34 +0500

    birinchi mavzu boyicha yechim

---
+++ 1-mavzu/O'zgaruvchilar.py
@@ -0,0 +1,306 @@
+# 1-masala.
+# a=4
+# p=4*a
+# print(f"tomoni {a}ga teng kvadratning permetri: {p}")
+
+
+# 2-masala.
+# a=3
+# s=a**2
+# print(f"Tomoni {a} ga teng kvadratning yuzi : {s} ga teng ")
+
+
+# 3-masala.
+# a=int(input("a = "))
+# b=int(input("b = "))
+# p=2*(a + b)
+# s=a*b
+# print(f"a tomon: {a} ga , b tomon: {b} ga teng \nYuzi: {s} ga , Peremetri: {p} ga teng")
+
+
+# 4-masala.
+# d = 8
+# pi = 3.14
+# l=pi*d
+# print(f"Aylana uzunligi: {l}")
+
+
+# 5-masala.
+# a=int(input("a = "))
+# v=a**3
+# s=6*a**2
+# print(f"Yo'la sirti: {s} ga, Hajmi: {v} ga teng.")
+
+
+# 6-masala.
+# a=int(input("a = "))
+# b=int(input("b = "))
+# d=int(input("d = "))
+# v = a*b*d
+# s = 2*(a*b+b*d+d*a)
+# print(f"To'la sirti: {s} ga, Hajmi: {v} ga teng ")
+
+
+# 7-masala.
+# r=int(input("Doira radyusini kiriting: "))
+# pi =3.14
+# l=2*pi*r
+# s=pi*r**2
+# print(f"Doir yuzi: {s} ga , Uzunligi: {l} ga teng")
+
+
+# 8-masala.
+# a=4
+# b=9
+# orta = (a+b)/2
+# print(f"Ikki sonning o'rta arfmetigi: {orta}")
+
+
+# 9-masala.
+# a=int(input("Birinchi sonini kiriting: "))
+# b=int(input("Ikkinchi sonini kiriting: "))
+# orta_g = (a*b)**0.5
+# print(f"Ikki sonnig o'rta geometrigi: {orta_g}")
+
+
+# 10-masala.
+# a = int(input("a = "))
+# b = int(input("b = "))
+# yig = a + b
+# kv_a = a **2
+# kv_b = b **2
+# kop = a * b
+# print(f"Ikki son ko'paytmasi: {kop} ga , \nYig'indisi: {yig} ga , 1-son kvsi: {kv_a} 2-son kv: {kv_b} ga teng")
+
+
+# 11-masala.
+# a = -11
+# b = 24
+# yig = a + b
+# kop = a * b
+# kv_a = a ** 2
+# kv_b = b ** 2
+# print(f"Yig'indi: {yig}, Ko'paytma: {kop}, Kv a: {kv_a}, Kv b: {kv_b} ga teng!")
+
+
+# 12-masala.
+# a = int(input("a = "))
+# b = int(input("b = "))
+# c = (a**2+b**2)**0.5
+# p = a+b+c
+# print(f"Gipatunuzasi: {c} ga , Peremetri: {p} ga teng")
+
+
+# 13-masala.
+# r1 = int(input("R1 = "))
+# r2 = int(input("R2 = "))
+# pi = 3.14
+# s3 = pi*(r1-r2)
+# print(f"Yuzlar ayrmasi: {s3} ga teng")
+
+
+# 14-masala.
+# l=18
+# pi = 3.14
+# r = l/(2*pi)
+# s = pi*r**2
+# print(f"Radyusi: {r} ga , Yuzi: {s} ga teng")
+
+
+# 15-masala.
+# s = 16
+# pi = 3.14
+# r = (s/pi)**0.5
+# d = 2*r
+# print(f"Radyusi: {r} ga , Diametri: {d} ga teng")
+
+
+# 16-masala.
+# x1 = int(input("x1 = "))
+# x2 = int(input("x2 = "))
+# x = abs(x1-x2)
+# pprint(f"Sonlar o'qidagi ikki nuqta orasidagi masofa: {x}")
+
+
+# 17-masala.
+# a=-6
+# b=10
+# c=14
+# ab = abs(a-b)
+# ac = abs(c-a)
+# print(ab, ac)
+
+
+# 18-masala.
+# a = int(input("a = "))
+# b = int(input("b = "))
+# c = int(input("c = "))
+# ac = abs(a-c)
+# cb = abs(c-b)
+# print(f"ac kesma uzunligi: {ac} ga , cb kesma uzunligi: {cb} ga teng.\nUzunliklarning ko'paytmasi: {ac*cb}ga teng")
+
+
+# 19-masala.
+# x1 = int(input("x1 = "))
+# x2 = int(input("x2 = "))
+# y1 = int(input("y1 = "))
+# y2 = int(input("y2 = "))
+# a = abs(x1 - x2)
+# b = abs(y2 - y1)
+# p = 2*(a + b)
+# s = a * b
+# print(f"To'gri to'rtburchakning peremetri: {p} ga.\nYuzasi: {s} ga teng ")
+
+
+# 20-masala.
+# x1 = int(input("x1 = "))
+# x2 = int(input("x2 = "))
+# y1 = int(input("y1 = "))
+# y2 = int(input("y2 = "))
+# d = ((x2-x1)**2 + (y2-y1)**2)**0.5
+# print(f"Berilgan ikki nuqta orasidagi masofa: {d}")
+
+
+# 21-masala.
+# x1, x2, x3 = 1, 6, 8
+# y1, y2, y3 = 2, 7, 10
+# a = ((x1-x2)**2+(y1-y2)**2)**0.5
+# c = ((x1-x3)**2+(y1-y3)**2)**0.5
+# b = ((x3-x2)**2+(y3-y2)**2)**0.5
+# p = (a+b+c)/2
+# s = (p*(p-a)*(p-b)*(p-c))**0.5
+# print(f"Peremteri: {p}, Yuzi: {s} ga teng")
+
+
+# 22-masala.
+# a = int(input("a = "))
+# b = int(input("b = "))
+# a , b = b , a
+# print(f"Aning qiymati : {a}, Bning qiymati: {b}")
+
+
+# 23-masala.
+# a = int(input("a = "))
+# b = int(input("b = "))
+# c = int(input("c = "))
+# a, c, b = b, a, c
+# print(f"a = {a}, b = {b}, c = {c}")
+
+
+# 24-masala.
+# a = int(input("a = "))
+# b = int(input("b = "))
+# c = int(input("c = "))
+# print(f"Boshida: a, b, c :{a, c, b}")
+# a, c, b =c, b, a
+# print(f"Keyingi: a, b, c: {a, c, b}")
+
+
+# 25-masala.
+# x = int(input("x="))
+# y = 3*x**6-6*x**2-7
+# print(y)
+
+
+# 26-masala.
+# x = int(input("x="))
+# y = 4*(x-3)**6-7*(x-3)**3+2
+# print(y)
+
+
+# 27-masala.
+# a = int(input("a="))
+# print(a**2, a**4, a**8)
+
+
+# 28-masala.
+# a = int(input("a="))
+# print(a**2, a**3, a**5, a**10, a**15)
+
+
+
+# 29-masala.
+# bur = int(input("a burchakni kiriting: "))
+# pi = 3.14
+# rad = (pi/180)*bur
+# print(f"{bur} gradus radianda {rad} radianga teng")
+
+
+# 30 - masala.
+# rad = float(input("Radianni kiriting: "))
+# pi = 3.14
+# bur = (180/pi)*rad
+# print(f"Kiritgan {rad} radianigiz {bur} gradusga to'g'ri keladi")
+
+
+
+# 31-masala.
+# tf = int(input("Tempura turani kiriting: "))
+# tc = (tf - 32)*5/9
+# print(tc)
+
+
+# 32-masala.
+# tf = int(input("Tempura turani kiriting: "))
+# tc = (tf-32)*5/9
+# print(tc)
+
+
+# 33-masala.
+# y = float(input("Konfetning massasni kiritng: "))
+# s =250/10
+# ys = y*s
+# print(f"1 kg konfet {s} ming so'm turadi. \n{y} kg konfet {ys} ming so'm bo'ladi")
+
+
+# 34-masala.
+# x = int(input("Konfedninig massasni kiriting: "))
+# a = int(input(f"{x} kg konfed narxni kiriting: "))
+# y = int(input("shokoladning massasni kiriting: "))
+# b = int(input(f"{y} kg shokolad narxni kiriting: "))
+# kon = a/x
+# shok = b/y
+# print(f"{x} kg konfed {a} ming so'm bo'lsa, 1 kg konfed {kon} ming so'm turadi")
+# print(f"{y} kg shokolad {b} ming so'm bo'alsa, 1 kg shokolad {shok} ming so'm turadi")
+
+
+
+# 35-masala.
+# vqay = float(input("Qayiqning tezligini kiriting: "))
+# vdar = float(input("Daryo oqiming tezligini kiriting: "))
+# t1 = float(input("Qayiqning oqim bo'ylab yurgandagi vaqti: "))
+# t2 = float(input("Qayiqning oqimga qarshi yurgandagi vaqti: "))
+# s1 = (vdar+vqay)*t1
+# s2 = (vqay-vdar)*t2
+# s = s1+s2
+# print(f"Qayiqning jami bosgan yo'li : {s} ga teng")
+
+
+# 36-masala.
+# v1 = int(input("Birinchi aftomobilning tezligni kiriting: "))
+# v2 = int(input("Ikkinchi aftomobilning tezligni kiriting: "))
+# s = int(input("Ikki aftomobil orasidagi masofa: "))
+# t = int(input("Uzoqlasha boshlagandan keyingi vaqt: "))
+# s_ora = s + t*(v2+v1)
+# print(f"Aftomobillar o'rtasidagi masofa: {s_ora}")
+
+
+
+# 37-masala.
+# v1 = int(input("Birinchi aftomobilning tezligni kiriting: "))
+# v2 = int(input("Ikkinchi aftomobilning tezligni kiriting: "))
+# s = int(input("Ikki aftomobil orasidagi masofa: "))
+# t = int(input("Yaqinlasha boshlagandan keyingi vaqt: "))
+# s_or = abs(s - (v1+v2)*t)
+# print(f"Aftomobillar orasidagi masofa: {s_or}")
+
+
+# 38-masala.
+# a = float(input("a="))
+# b = float(input("b="))
+# x = -b/a
+# print(f"Yechimi: {x}")
+
+
+
+# 39-masala.
